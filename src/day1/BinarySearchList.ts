581481bd2c42109cd4bb7c1f93751e1e3ad064ac
1Bishop <chris@bishop.dev>
2024-02-27 09:40:10 -0500

    Implement binary search in bs_list function
    The functionality of the bs_list function has been expanded to perform a binary search. The algorithm was implemented directly into the function, looping until the target value (needle) is found in the input array (haystack) or it becomes clear that the value is not present.

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0
+    let hi = haystack.length
+    let mid = Math.floor((low + hi) / 2)
+    do {
+        const mid = Math.floor((low + hi) / 2)
+        if (haystack[mid] === needle) {
+            return true;
+        } else if (haystack[mid] < needle) {
+            low = mid + 1;
+        } else {
+            hi = mid ;
+        }
+    } while (low < hi)
 
+    return false
 }
